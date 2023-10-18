bbbe7b05a2887dca66c32022c5ba8e1b5cece247
HossamAhmedGH <hossam.ahmed.abdelwahab@gmail.com>
2023-10-18 08:46:05 -0700

    main.h"

---
+++ main.h
@@ -0,0 +1,13 @@
+#ifndef  _MAIN_H_
+#define  _MAIN_H_
+
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <sys/stat.h>
+#include <errno.h>
+#include <fcntl.h>
+#include <signal.h>
+#include <limits.h>
